5eeb35b678fb7f00e899336ed38399261293443c
htariku <hgtariku@yahoo.com>
2022-02-22 12:52:16 -0500

    logot js and button implemented into files and managed front end js logic in an MVC app

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers/');
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 
 const app = express();
